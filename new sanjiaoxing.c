ea74557e9213e0ee0f6d72d883148d98bf82b9b1
wbsnt139 <1329880680@qq.com>
2022-11-08 17:56:48 +0800

    new sanjiaoxing

---
+++ new sanjiaoxing.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+
+int main()
+{
+	int i, j, *pn,n,  *pa,a;
+	pn = &n, pa = &a;
+long int b=0,sum=0;
+	scanf("a=%d,n=%d", pa, pn);
+	
+	for (i = 0; i < n; i++)
+	{
+		b = b+a;
+		 printf("%ld", b);
+		
+		printf("\n");
+		sum = b + sum; 
+		a *= 10;
+		
+
+	}
+	printf("sum=%ld", sum);
+	return 0;
+}
