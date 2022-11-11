c7df4f38be981ee67e82e0acd5e06c0856ab13dd
wbsnt139 <1329880680@qq.com>
2022-11-11 12:04:19 +0800

    Create shi ge zheng shu an ni xu shuchu.c

---
+++ shi ge zheng shu an ni xu shuchu.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i, m, n;
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &a[i]);
+	}
+	i--;			
+	for (i; i > -1; i--) {
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
