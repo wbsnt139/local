5b5117ebce084c3e80d9b24f155949dd7e3e85b5
wbsnt139 <1329880680@qq.com>
2022-11-14 18:05:38 +0800

    Create 快递作业3.c

---
+++ 快递作业3.c
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct kuaidiren
+{
+	char human[50];
+};
+struct city
+{
+	char d[50];
+};
+struct time
+{
+	char year[50];
+};
+int main()
+{
+	char a [30];
+	gets(a);    //输入字符串a
+	if (strcmp(a, "快递单号:10202") == 0)
+	
+		printf("快递单号:10202\n" "发货方:Xiao Li,13601110123,Xiangjiang KLMY\n"
+
+			"接收方:Xiao Wang,1330000666,Beijing XC\n"
+
+			"始发城市:kelamayi\n"
+
+			"目的城市:Beijing\n"
+
+			"快递服务商:Fengxingxia\n"
+
+			"发送时间:2022.11.11\n"
+
+			"接收时间:2022.11.16\n"
+
+			"快递价格:24");
+	
+	else
+		printf("no found.");
+
+//判断是否为真
+		
+
+	/*char sevice[50];
+	char price[50];
+	char num[50];*/
+	/*scanf("快递单号:%[^\n]\n", num);
+	struct kuaidiren fachu;
+	scanf("发货方:%[^\n]\n", fachu.human);
+	struct kuaidiren receiver;
+	scanf("接收方:%[^\n]\n", receiver.human);
+	struct city start;
+	scanf("始发城市:%[^\n]\n", start.d);
+	struct city goal;
+	scanf("目的城市:%[^\n]\n", goal.d);
+	scanf("快递服务商:%[^\n]\n", sevice);
+	struct time send;
+	scanf("发送时间:%[^\n]\n", send.year);
+	struct time receive;
+	scanf("接收时间:%[^\n]\n", receive.year);
+	scanf("快递价格:%[^\n]", price);*/
+	/*printf("快递单号:%s\n发货方:%s\n接收方:%s\n始发城市:%s\n目的城市:%s\n快递服务商:%s\n发送时间:%s\n接收时间:%s\n", num, fachu.human, receiver.human, start.d, goal.d, sevice, send.year, receive.year);
+	printf("快递价格:%s", price);*/
+	return 0;
+}
