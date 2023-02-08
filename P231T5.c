1d386f654d1ccc15c7bf0707e0c66a3534ee8eab
wbsnt139 <1329880680@qq.com>
2023-02-08 23:32:16 +0800

    Update P231T5.c

--- P231T5.c
+++ P231T5.c
@@ -1,17 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void fun(int k)
+//void fun(int k)
+//{
+//	if (k > 0)
+//	{
+//		fun(k - 1);
+//	}
+//	printf(" %d,", k);
+//}
+////为何有0，后的1 2 3？
+//int main()
+//{
+//	int w = 3;
+//	fun(w);
+//	printf("\n");
+//}
+double fun(int a, int b, int c)
 {
-	if (k > 0)
-	{
-		fun(k - 1);
-	}
-	printf(" %d,", k);
+	double s;
+	s = a / b * c;
+	return s;
 }
-//为何有0，后的1 2 3？
 int main()
 {
-	int w = 3;
-	fun(w);
-	printf("\n");
+	int a = 18, b = 4, c = 6;
+	double d;
+	d = fun(a, b, c);
+	printf("%4.1lf", d);
 }
